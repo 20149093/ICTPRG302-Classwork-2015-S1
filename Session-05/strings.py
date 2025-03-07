0646e3ef31ae3f187971b55fe33ef67b5baa9f60
baltimoreWhite <baltimorewhite@gmail.com>
2025-03-07 13:33:33 +0800

    new session5

---
+++ Session-05/strings.py
@@ -0,0 +1,18 @@
+# Filename: Strings-revisited.py
+# Location: Session-05
+
+tic_tac = "- X 0\n" + "X - X\n" + "0 - 0\n"
+
+
+# Write the code to print the length of the string
+print(len(tic_tac))
+
+# Write the code to print ONLY the 13th character
+print(tic_tac[12])
+
+# Write the code to show only the first 5 character
+
+print(tic_tac[0:5])
+
+# Write the code to show characters 8 to 10
+print(tic_tac[7:10])
